3a60280a0c481c8974210a7f05f7ab31f7d8a737
Lauren <wolfela@uw.edu>
2016-11-18 20:12:01 -0800

    swap checking

--- hw4/game_model.cpp
+++ hw4/game_model.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <array>
 #include "game_model.h"
 
 using namespace std;
@@ -27,7 +28,7 @@ GameModel::GameModel() {
 
     game_id_ = 12345;
     total_moves_ = 20;
-    moves_left_ = 20;
+    moves_made_ = 0;
     num_colors_ = 6;
 }
 
@@ -35,14 +36,22 @@ GameModel::GameModel(string filepath) {
     DeserializeGame(filepath);
 }
 
+int GameModel::GetMovesRemaining() const {
+    return total_moves_ - moves_made_;
+}
+
+int GameModel::GetSelectedCandyIdx() const {
+    return sel_candy_idx_;
+}
+
+CandyPtr GameModel::GetSelectedCandyPtr() const {
+    return (CandyPtr) game_board_->data[sel_candy_idx_];
+}
+
 int GameModel::GetCandyColor(const int &idx) const {
     CandyPtr candy;
     candy = (CandyPtr) game_board_->data[idx];
     return candy->color;
-}
-
-CandyPtr GameModel::GetSelectedCandy() const {
-    return sel_candy_;
 }
 
 int GameModel::GetRowLength() const {
@@ -53,13 +62,62 @@ int GameModel::GetColLength() const {
     return game_board_->num_cols;
 }
 
+int GameModel::ConvertToRow(const int &idx) const {
+    return idx / GetRowLength();
+}
+
+int GameModel::ConvertToCol(const int &idx) const {
+    return idx % GetRowLength();
+}
+
+int GameModel::ConvertToIdx(const int &row, const int &col) const {
+    return row * GetColLength() + col;
+}
+
 int GameModel::GetBoardSize() const {
     return game_board_->num_rows * game_board_->num_cols;
 }
 
-bool GameModel::SwapCandy(const char &dir) {
+void GameModel::SetSelectedCandy(int idx) {
+    sel_candy_idx_ = idx;
+}
 
-    //TODO
+bool GameModel::SwapCandy(const char &dir) {
+    int row1, row2, col1, col2, idx1, idx2;
+
+    if(GetSelectedCandyPtr() == nullptr) {
+        return false;
+    }
+
+    row1 = row2 = ConvertToRow(sel_candy_idx_);
+    col1 = col2 = ConvertToCol(sel_candy_idx_);
+
+    if(dir == 'N') {
+        row2++;
+    } else if(dir == 'S') {
+        row2--;
+    } else if(dir == 'E') {
+        col2--;
+    } else if(dir == 'W') {
+        col2++;
+    } else {
+        return false;
+    }
+
+    if(col2 < 0 || row2 < 0 || row2 >= GetRowLength() || col2 >= GetColLength()) {
+        return false;
+    }
+
+    idx1 = sel_candy_idx_;
+    idx2 = ConvertToIdx(row2, col2);
+
+    if(TrySwap(idx1, idx2)) {
+        moves_made_++;
+        SetSelectedCandy(NO_CANDY);
+        FireBoardLoop();
+        return true;
+    }
+
     return false;
 }
 
@@ -78,8 +136,85 @@ void GameModel::SerializeGame(const string &filepath) {
 
 }
 
-bool GameModel::IsValidSwap(const int &idx1, const int &idx2) {
-    //TODO
+bool GameModel::TrySwap(const int &idx1, const int &idx2) {
+    bool matched;
+    Swap(game_board_, idx1, idx2);
+    matched = HasVerticalMatch(idx1) || HasVerticalMatch(idx2) ||
+         HasHorizontalMatch(idx1) || HasHorizontalMatch(idx2);
+    if(!matched) {
+        Swap(game_board_, idx1, idx2);
+    }
+    return matched;
+}
+
+// These could probably be optimized to work with any minimum match length,
+// but works for now
+bool GameModel::HasVerticalMatch(const int &idx) {
+    int size, arr_idx, seq_count, color;
+    size = GetColLength();
+    arr_idx = 0, seq_count = 0, color = -2;
+    int candy_seq[size];
+
+    if(GetRowLength() >= MIN_MATCH_LENGTH) {
+        //for every valid candy index above and below given index,
+        //add candy color to vector
+        for(int i = 0; i < idx + (GetRowLength() * MIN_MATCH_LENGTH);
+            i + GetRowLength()) {
+            if(i > 0 && i < GetBoardSize()) {
+                candy_seq[arr_idx] = GetCandyColor(i);
+                arr_idx++;
+            }
+        }
+
+        for(int i = 0; i < arr_idx; i++) {
+            //Found sequence!
+            if(seq_count == MIN_MATCH_LENGTH) {
+                return true;
+            }
+
+            if(candy_seq[i] == color) {
+                //matches sequence
+                seq_count++;
+            } else {
+                //discontinuous
+                color = candy_seq[i];
+                seq_count = 1;
+            }
+        }
+        //check for last element satisfy minimum match length
+        if(seq_count == MIN_MATCH_LENGTH) {
+            return true;
+        }
+    }
+    return false;
+}
+
+// These could probably be optimized to work with any minimum match length,
+// but works for now
+bool GameModel::HasHorizontalMatch(const int &idx) {
+    int col = ConvertToCol(idx);
+
+    if(GetColLength() >= 3) {
+        if(col >= 2 &&
+            (GetCandyColor(idx - 2)
+            == GetCandyColor(idx - 1)
+            == GetCandyColor(idx))) {
+            // 2 left + idx + 0 right
+            return true;
+        } else if(col >= 1 && col + 1 < GetColLength() &&
+            (GetCandyColor(idx - 1)
+            == GetCandyColor(idx)
+            == GetCandyColor(idx + 1))) {
+            // 1 left + idx + 1 right
+            return true;
+        } else if(col + 2 < GetColLength() &&
+            (GetCandyColor(idx)
+            == GetCandyColor(idx + 1)
+            == GetCandyColor(idx + 2))) {
+            // 0 left + idx + 2 right
+            return true;
+        }
+    }
     return false;
 }
 
--- hw4/game_model.h
+++ hw4/game_model.h
@@ -21,24 +21,31 @@ class GameModel {
         GameModel();
         GameModel(string filepath);
 
-        int GetMovesRemaining() const { return moves_left_; };
         int GetScore() const { return score_; };
         int GetGameID() const { return game_id_; };
 
-        CandyPtr GetSelectedCandy() const;
+        int GetMovesRemaining() const;
+        int GetSelectedCandyIdx() const;
+        CandyPtr GetSelectedCandyPtr() const;
         int GetCandyColor(const int &idx) const;
         int GetRowLength() const;
         int GetColLength() const;
+        int ConvertToRow(const int &idx) const;
+        int ConvertToCol(const int &idx) const;
+        int ConvertToIdx(const int &row, const int &col) const;
         int GetBoardSize() const;
+
+        void SetSelectedCandy(int idx);
         bool SwapCandy(const char &dir);
         bool IsGameOver();
 
     private:
-
         void DeserializeGame(const string &filepath);
         void SerializeGame(const string &filepath);
 
-        bool IsValidSwap(const int &idx1, const int &idx2);
+        bool TrySwap(const int &idx1, const int &idx2);
+        bool HasVerticalMatch(const int &idx);
+        bool HasHorizontalMatch(const int &idx);
         void FireBoardLoop();
         void FindMatches();
         void FindVerticalMatches(const int &num);
@@ -51,12 +58,14 @@ class GameModel {
         Array2D game_board_; //current visible board
         Array2D fired_state_; //how many times each cell needs to fire before win
         vector<int> extension_offset_; //row number for next drop position of extension board
-        CandyPtr sel_candy_ = nullptr; //button-selected candy -- TODO: need to delete after swap
+        int sel_candy_idx_ = -1; //button-selected candy
         int game_id_;
         int total_moves_;
-        int moves_left_;
+        int moves_made_;
         int num_colors_; //number of colors for this game board
         int score_ = 0;
+        const int NO_CANDY = -1;
+        const int MIN_MATCH_LENGTH = 3;
 };
 
 #endif // _GAME_MODEL_H_
